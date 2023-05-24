59d47c37a7a09d8c59c75d77df325529e3c3e4af
CRESPY Arthur <arthur@crespy.eu>
2023-05-24 17:45:39 +0200

    Work on the env_replace function and add env_size calculator

--- files/srcs/env_replace.c
+++ files/srcs/env_replace.c
@@ -12,11 +12,33 @@
 
 #include "../includes/minishell.h"
 
-size_t	ft_env_replace_size(t_data *data, char *command)
+// Definitely need improvement
+int	ft_env_size(t_data *data, const char *command)
 {
-	(void)data;
-	(void)command;
-	return (1000);
+	int		i;
+	int		j;
+	int		size;
+	char	*tmp;
+
+	i = 0;
+	size = 0;
+	while (command[i])
+	{
+		if (command[i++] == '$')
+		{
+			j = 0;
+			tmp = malloc(sizeof(char) * (ft_strlen(command)));
+			if (!tmp)
+				ft_exit(data, MALLOC_ERROR, "ft_env_size malloc error");
+			while (command[i] && command[i] != ' ' && command[i] != '\x1F')
+				tmp[j++] = command[i++];
+			tmp[j] = '\0';
+			size += (ft_strlen(data->env[ft_tabchr(data->env, tmp)]) + j) + 3;
+			free(tmp);
+		}
+		i++;
+	}
+	return (size);
 }
 
 void	env_save(t_data *data, int argc, char **argv, char **envp)
@@ -24,20 +46,6 @@ void	env_save(t_data *data, int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 	data->env = envp;
-}
-
-void	ft_env_check(const char *command, char *tmp, int *i, int *j)
-{
-	int	check;
-
-	check = 0;
-	(*i)++;
-	while (command[(*i)] && command[(*i)] != ' ' && command[(*i)] != '$'
-		&& command[(*i)] != '\'' && command[(*i)] != '\"')
-	{
-		tmp[(*j)++] = command[(*i)++];
-		check = 1;
-	}
 }
 
 char	*ft_env_replace(t_data *data, char *command)
@@ -48,16 +56,21 @@ char	*ft_env_replace(t_data *data, char *command)
 
 	i = 0;
 	j = 0;
-	tmp = malloc(sizeof(char *) * ft_env_replace_size(data, command));
+	tmp = malloc(sizeof(char) * (ft_env_size(data, command) + 1));
 	if (!tmp)
 		ft_exit(data, MALLOC_ERROR, "ft_env_replace malloc error");
 	while (command[i])
 	{
 		if (command[i] == '$')
-			ft_env_check(command, tmp, &i, &j);
+		{
+			else
+			{
+				while (command[i] && command[i] != ' ' && command[i] != '\x1F')
+					tmp[j++] = command[i++];
+			}
+		}
 		else
 			tmp[j++] = command[i++];
-		printf("tmp[%d] = %c\n", j - 1, tmp[j - 1]);
 	}
 	tmp[j] = '\0';
 	free(command);
