3bd3b8d184eb6e9c3697fd11db52691b73338ad6
CRESPY Arthur <arthur@crespy.eu>
2023-05-24 14:18:56 +0200

    Replace ' and " separately in order to process $ between them

--- files/srcs/command_parsing.c
+++ files/srcs/command_parsing.c
@@ -22,7 +22,8 @@ void	command_parsing(t_data *data, char *command)
 	command = ft_char_replace(data, command, '\n');
 	command = ft_char_replace(data, command, '\v');
 	command = ft_operators_replace(data, command);
-	command = ft_quotes_replace(data, command);
+	command = ft_quotes_replace(data, command, '\'');
+	command = ft_quotes_replace(data, command, '\"');
 	data->command = ft_split(*data, command, '\x1F');
 	while (data->command[i])
 	{
