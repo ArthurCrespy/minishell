3cfb4d6aa25f588c257ce768f6716c7a0a30e54f
CRESPY Arthur <arthur@crespy.eu>
2023-06-05 15:04:51 +0200

    Add environment saver

--- files/srcs/env_save.c
+++ files/srcs/env_save.c
@@ -14,8 +14,21 @@
 
 void	env_save(t_data *data, int argc, char **argv, char **envp)
 {
+	int		i;
+	char	**env;
+
+	i = 0;
 	(void)argc;
 	(void)argv;
-	data->env = envp;
+	if (!envp || !envp[0])
+		ft_exit(data, ENV_ERROR, "minishell need an valid working environment");
+	env = malloc(sizeof(char *) * ft_tablen(envp));
+	if (!env)
+		ft_exit(data, MALLOC_ERROR, "env_save malloc error");
+	while (envp[i])
+	{
+		data->env[i] = ft_strdup(*data, envp[i]);
+		i++;
+	}
 	data->return_value = 0;
 }
