3182aec97936a4cb25eb9a91711acafe2611caea
CRESPY Arthur <arthur@crespy.eu>
2023-06-06 15:53:13 +0200

    Separate global variable

--- files/includes/minishell_global.h
+++ files/includes/minishell_global.h
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   minishell_global.h                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/19 12:31:36 by acrespy           #+#    #+#             */
-/*   Updated: 2023/05/19 12:31:36 by acrespy          ###   ########.fr       */
+/*   Created: 2023/06/06 15:46:33 by acrespy           #+#    #+#             */
+/*   Updated: 2023/06/06 15:46:36 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef MINISHELL_GLOBAL_H
+# define MINISHELL_GLOBAL_H
 
-# include "./minishell_include.h"
-# include "./minishell_define.h"
-# include "./minishell_typedef.h"
-# include "./minishell_struct.h"
-# include "./minishell_global.h"
-# include "./minishell_cmd.h"
-# include "./minishell_cmd_utils.h"
+t_data *g_data_access;
 
 #endif
