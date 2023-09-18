295dff3d5b2ac2711aac593d5cdad69656a4e119
CRESPY Arthur <arthur@crespy.eu>
2023-09-18 13:03:51 +0200

    Prepare the new parsing

--- files/includes/minishell_define.h
+++ files/includes/minishell_define.h
@@ -18,6 +18,20 @@
 # define TCSETATTR_ERROR 3
 # define ENV_ERROR 4
 
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
+
+# define CMD 0 		    // command
+# define SEMICOLON 1    // ;
+# define AND 2          // &&
+# define OR 3           // ||
+# define PIPE 4         // |
+# define REDIR_IN 5     // <
+# define REDIR_OUT 6    // >
+# define REDIR_APPEND 7 // >>
+# define DELIMITER 8    // <<
+
 # define BUFFER_SIZE 1024
 
 #endif
