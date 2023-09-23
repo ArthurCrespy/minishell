594aa07c93dd519bb8336c225b4d75741cfdb709
CRESPY Arthur <arthur@crespy.eu>
2023-09-23 15:56:41 +0200

    Norm

--- files/srcs/builtins/cd.c
+++ files/srcs/builtins/cd.c
@@ -12,46 +12,16 @@
 
 #include "../../includes/minishell.h"
 
-// cd sans options :
-//					si HOME est empty alors rien
-//					si HOME est defini alors HOME
-// cd avec options :
-//					si l'arg commence par / alors chemin absolu
-//					si l'arg commence par . alors chemin relatif
-//						dans le dossier actuel
-//					si l'arg commence par .. alors chemin relatif,
-//						dans le dossier precedent
-//
+/*  cd sans options :
+		si HOME est empty alors rien
+		si HOME est defini alors HOME
+	cd avec options :
+		si l'arg commence par / alors chemin absolu
+		si l'arg commence par . alors chemin relatif dans le dossier actuel
+		si l'arg commence par .. alors chemin relatif dans le dossier precedent
+*/
 
-// Proposition builtin cd :
 /*int	ft_cd(t_data *data, t_exec *exec)
-{
-	if (!exec->args[0])
-	{
-		if (!chdir(env_return(data, "HOME")))
-		{
-			env_update(data, "OLDPWD", env_return(data, "PWD"));
-			env_update(data, "PWD", getcwd(NULL, 0));
-		}
-		else
-			return (perror("cd"), 1);
-	}
-	else if (exec->args_nb == 1)
-	{
-		if (!chdir(exec->args[0]))
-		{
-			env_update(data, "OLDPWD", env_return(data, "PWD"));
-			env_update(data, "PWD", getcwd(NULL, 0));
-		}
-		else
-			return (perror("cd"), 1);
-	}
-	else
-		return (printf("cd: string not in pwd: %s\n", exec->args[0]), 1);
-	return (0);
-}*/
-
-int	ft_cd(t_data *data, t_exec *exec)
 {
 	if (exec->args_nb == 0)
 	{
@@ -84,5 +54,33 @@ int	ft_cd(t_data *data, t_exec *exec)
 			env_update(data, "PWD", getcwd(NULL, 0));
 		}
 	}
+	return (0);
+}*/
+
+// Proposition builtin cd :
+int	ft_cd(t_data *data, t_exec *exec)
+{
+	if (!exec->args[0])
+	{
+		if (!chdir(env_return(data, "HOME")))
+		{
+			env_update(data, "OLDPWD", env_return(data, "PWD"));
+			env_update(data, "PWD", getcwd(NULL, 0));
+		}
+		else
+			return (perror("cd"), 1);
+	}
+	else if (exec->args_nb == 1)
+	{
+		if (!chdir(exec->args[0]))
+		{
+			env_update(data, "OLDPWD", env_return(data, "PWD"));
+			env_update(data, "PWD", getcwd(NULL, 0));
+		}
+		else
+			return (perror("cd"), 1);
+	}
+	else
+		return (printf("cd: string not in pwd: %s\n", exec->args[0]), 1);
 	return (0);
 }
