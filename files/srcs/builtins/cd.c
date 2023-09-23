d81bc61bcf2db190218daba09c67f3b68d112f2a
CRESPY Arthur <arthur@crespy.eu>
2023-09-23 14:33:32 +0200

    Proposition builtin cd

--- files/srcs/builtins/cd.c
+++ files/srcs/builtins/cd.c
@@ -23,13 +23,41 @@
 //						dans le dossier precedent
 //
 
+// Proposition builtin cd :
+/*int	ft_cd(t_data *data, t_exec *exec)
+{
+	if (!exec->args[0])
+	{
+		if (!chdir(env_return(data, "HOME")))
+		{
+			env_update(data, "OLDPWD", env_return(data, "PWD"));
+			env_update(data, "PWD", getcwd(NULL, 0));
+		}
+		else
+			return (perror("cd"), 1);
+	}
+	else if (exec->args_nb == 1)
+	{
+		if (!chdir(exec->args[0]))
+		{
+			env_update(data, "OLDPWD", env_return(data, "PWD"));
+			env_update(data, "PWD", getcwd(NULL, 0));
+		}
+		else
+			return (perror("cd"), 1);
+	}
+	else
+		return (printf("cd: string not in pwd: %s\n", exec->args[0]), 1);
+	return (0);
+}*/
+
 int	ft_cd(t_data *data, t_exec *exec)
 {
 	if (exec->args_nb == 0)
 	{
 		if (chdir(env_return(data, "HOME")))
 		{
-			perror("cd: ");
+			perror("cd");
 			return (1);
 		}
 		else
@@ -47,7 +75,7 @@ int	ft_cd(t_data *data, t_exec *exec)
 	{
 		if (chdir(exec->args[0]))
 		{
-			perror("cd: ");
+			perror("cd");
 			return (1);
 		}
 		else
