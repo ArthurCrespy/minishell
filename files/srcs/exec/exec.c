4876b0122bb4854c0a6fa851367c41c970e3087f
CRESPY Arthur <arthur@crespy.eu>
2023-09-24 16:01:50 +0200

    Modify exec

--- files/srcs/exec/exec.c
+++ files/srcs/exec/exec.c
@@ -14,13 +14,19 @@
 
 void	exec(t_data *data)
 {
-	(void)data;
+	int		i;
+
+	i = 0;
 	if (data->exec_launch == false)
 	{
 		printf("minishell: parse error\n");
 		return ;
 	}
-	find_cmd(data, data->exec[0]);
+	while (data->exec[i])
+	{
+		find_cmd(data, data->exec[i]);
+		i++;
+	}
 }
 
 void	find_cmd(t_data *data, t_exec *exec)
@@ -30,16 +36,19 @@ void	find_cmd(t_data *data, t_exec *exec)
 	cmd = exec->cmd;
 	if (!ft_strcmp(cmd, "exit"))
 		ft_ft_exit(data, data->exec[0]);
-	if (!ft_strcmp(cmd, "pwd"))
+	else if (!ft_strcmp(cmd, "pwd"))
 		ft_pwd(data);
-	if (!ft_strcmp(cmd, "cd"))
+	else if (!ft_strcmp(cmd, "cd"))
 		ft_cd(data, data->exec[0]);
-	if (!ft_strcmp(cmd, "unset"))
+	else if (!ft_strcmp(cmd, "unset"))
 		ft_unset(data, data->exec[0]);
-	if (!ft_strcmp(cmd, "echo"))
+	else if (!ft_strcmp(cmd, "echo"))
 		ft_echo(data, data->exec[0]);
-	if (!ft_strcmp(cmd, "export"))
+	else if (!ft_strcmp(cmd, "export"))
 		ft_export(data, data->exec[0]);
-	if (!ft_strcmp(cmd, "env"))
+	else if (!ft_strcmp(cmd, "env"))
 		ft_env(data);
 }
+
+// 	else
+//		ft_execve(data, data->exec[0]);
