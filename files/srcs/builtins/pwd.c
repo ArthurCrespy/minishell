53444308f695278ed901db9efdee28b538987e6f
CRESPY Arthur <arthur@crespy.eu>
2023-10-05 16:10:40 +0200

    Improve error management on pwd

--- files/srcs/builtins/pwd.c
+++ files/srcs/builtins/pwd.c
@@ -18,6 +18,8 @@ int	builtin_pwd(t_data *data)
 
 	working_directory = env_return(data, "PWD");
 	if (!working_directory)
+		working_directory = getcwd(NULL, 0);
+	if (!working_directory)
 		return (1);
 	else
 		ft_putstr_fd(working_directory, STDOUT_FILENO);
