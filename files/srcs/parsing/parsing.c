6865a55d7ab97977cf11fd22a82a90dcf0a9397b
CRESPY Arthur <arthur@crespy.eu>
2023-10-09 14:49:12 +0200

    Fix parsing when no command is found but there is redirs

--- files/srcs/parsing/parsing.c
+++ files/srcs/parsing/parsing.c
@@ -27,10 +27,13 @@ void	ft_exec_data_process(t_data *data, t_exec **exec, int *i, int *j)
 		data->pipes_nb++;
 		(*i)++;
 	}
-	if (data->command[(*i)][0] == '<' || data->command[(*i)][0] == '>')
+	while (data->command[(*i)] && (data->command[(*i)][0] == '<'
+		|| data->command[(*i)][0] == '>'))
 		ft_exec_token_parser(data, exec[(*j)], i);
-	exec[(*j)]->cmd = ft_strdup(data, data->command[(*i)]);
-	(*i)++;
+	if (data->command[(*i)] && data->command[(*i)][0] != '|')
+		exec[(*j)]->cmd = ft_strdup(data, data->command[(*i)]);
+	if (exec[(*j)]->cmd != NULL)
+		(*i)++;
 	while (data->command[(*i)] && data->command[(*i)][0] == '-')
 		exec[(*j)]->flags[exec[(*j)]->flags_nb++] = ft_strdup(data,
 				data->command[(*i)++]);
