a69b90dfea590a6bd2198a5824841a7ce760c55d
CRESPY Arthur <arthur@crespy.eu>
2023-10-09 16:58:19 +0200

    Fix norm (new cd builtin)

--- files/srcs/builtins/cd.c
+++ files/srcs/builtins/cd.c
@@ -14,33 +14,28 @@
 
 int	builtin_cd(t_data *data, t_exec *exec)
 {
-	if (exec->args_nb == 0)
+	char	*target_directory;
+
+	if (exec->args_nb > 1)
 	{
-		if (!chdir(env_return(data, "HOME")))
-		{
-			env_update(data, "OLDPWD", env_return(data, "PWD"));
-			env_update(data, "PWD", getcwd(NULL, 0));
-		}
-		else
-			return (perror("cd"), 1);
+		data->return_value = 1;
+		ft_putstr_fd("cd: too many arguments\n", 2);
+		return (1);
 	}
-	else if (exec->args_nb == 1)
+	if (exec->args_nb == 0)
+		target_directory = env_return(data, "HOME");
+	else
+		target_directory = exec->args[0];
+	if (!chdir(target_directory))
 	{
-		if (!chdir(exec->args[0]))
-		{
-			env_update(data, "OLDPWD", env_return(data, "PWD"));
-			env_update(data, "PWD", getcwd(NULL, 0));
-		}
-		else
-		{
-			data->return_value = 1;
-			return (perror("cd"), 1);
-		}
+		env_update(data, "OLDPWD", env_return(data, "PWD"));
+		env_update(data, "PWD", getcwd(NULL, 0));
 	}
 	else
 	{
 		data->return_value = 1;
-		return (ft_putstr_fd("minishell: cd: too many arguments\n", 2), 1);
+		perror("cd");
+		return (1);
 	}
 	return (0);
 }
